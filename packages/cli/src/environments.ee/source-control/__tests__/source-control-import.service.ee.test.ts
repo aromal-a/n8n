f64ea0060dcfbd5ca8190aa811df058f36a3cf9d
Ricardo Espinoza <ricardo@n8n.io>
2026-01-02 17:59:07 -0500

    Add data table to env feature

--- packages/cli/src/environments.ee/source-control/__tests__/source-control-import.service.ee.test.ts
+++ packages/cli/src/environments.ee/source-control/__tests__/source-control-import.service.ee.test.ts
@@ -79,6 +79,9 @@ describe('SourceControlImportService', () => {
 		sourceControlScopedService,
 		mock(),
 		mock(),
+		mock(),
+		mock(),
+		mock(),
 	);
 
 	const globMock = fastGlob.default as unknown as jest.Mock<Promise<string[]>, string[]>;
