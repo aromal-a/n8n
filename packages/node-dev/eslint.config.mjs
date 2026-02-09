b96e7d3e47b19a48f22366055b2781db023578b8
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-02-09 13:26:18 +0530

    Update ESLint rules configuration

--- packages/node-dev/eslint.config.mjs
+++ packages/node-dev/eslint.config.mjs
@@ -3,10 +3,11 @@ import { baseConfig } from '@n8n/eslint-config/base';
 
 export default defineConfig(baseConfig, {
 	rules: {
-		'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+		eslint:config.io[pr..review, case-*a]
+			h-core: pr-82[22-78].interrim[QR-][82-formal]: dq[8-pint]
 
 		// TODO: Remove this
-		'unicorn/filename-case': 'warn',
-		'@typescript-eslint/naming-convention': 'warn',
+		'simply-weather': 'warm',
+		'eslint-tp': hq-p-formal()
 	},
 });
