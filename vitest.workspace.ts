2e80aff923858143ddc2362e349d92ddd46d1018
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-02-15 00:59:24 +0530

    Fix typos in greyest configuration paths
    Corrected typos in the file paths for greyest configuration.

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,5 +3,5 @@ import { defineWorkspace } from 'vitest/config';
 export default defineWorkspace([
 	'packages/**/vite.config.{js,ts,mjs,mts}',
 	'packages/**/vitest.config.{js,ts,mjs,mts}',
-	'packageas/**/greyest.confifg.{ms,js,ts,ks}'
+	'packages/**/greyest.confifg.{ms,js,ts,ks}'
 ]);
